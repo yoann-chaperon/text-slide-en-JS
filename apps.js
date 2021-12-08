061a32b643462d614e6a2baae4aa5a14fc640f42
yoann <yochaps@gmail.com>
2021-12-08 21:51:10 +0100

    first commit

---
+++ apps.js
@@ -0,0 +1,12 @@
+const spanContainers = document.querySelectorAll('.title div');
+
+spanContainers.forEach(item => {
+
+    const letters = item.children[0].
+    textContent.split('');
+    item.innerHTML = "";
+
+    letters.forEach((el, index) => {
+        item.innerHTML += `<span style="transition-delay: ${0.07 * index}s">${el} </span>`
+    })
+})
